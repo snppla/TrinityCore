7bd5c481c74a0b115f8b81c498e09b75d165ec09
r00ty-tc <r00ty-tc@users.noreply.github.com>
2017-02-06 11:34:26 +0000

    More codestyle

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -6732,7 +6732,7 @@ bool ObjectMgr::SpawnCreatureGroup(uint32 groupId, Map* map, bool ignoreRespawn,
 
     for (ObjectGuid::LowType guid : spawnList)
     {
-        if (const CreatureData* cdata = GetCreatureData(guid))
+        if (CreatureData const* cdata = GetCreatureData(guid))
         {
             ASSERT(itr->second.mapId == cdata->mapid);
 
@@ -6839,7 +6839,7 @@ bool ObjectMgr::SpawnGOGroup(uint32 groupId, Map* map, bool ignoreRespawn, bool 
 
     for (ObjectGuid::LowType guid : golist)
     {
-        if (const GameObjectData* godata = GetGOData(guid))
+        if (GameObjectData const* godata = GetGOData(guid))
         {
             ASSERT(itr->second.mapId == godata->mapid);
 
